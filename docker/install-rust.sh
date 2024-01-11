81d6db93d0e531b3be48f96a782df78585779993
Benedikt Koenig <benedikt.koenig@agile-robots.com>
2024-01-11 14:38:33 +0100

    feat: reducing size of image

---
+++ docker/install-rust.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+set -eux
+
+# Install Rust
+RUSTUP_VERSION=1.26.0
+RUST_VERSION=1.75.0
+RUST_ARCH="x86_64-unknown-linux-gnu"
+
+RUSTUP_URL=https://static.rust-lang.org/rustup/archive/$RUSTUP_VERSION/$RUST_ARCH/rustup-init
+curl -o rustup-init $RUSTUP_URL
+
+export RUSTUP_HOME="/usr/local/rustup"
+export CARGO_HOME="/usr/local/cargo"
+export PATH="/usr/local/cargo/bin:$PATH"
+
+chmod +x rustup-init;
+./rustup-init -y --no-modify-path --default-toolchain $RUST_VERSION;
+rm rustup-init;
+chmod -R a+w $RUSTUP_HOME $CARGO_HOME
+
+cargo install cargo-c --version 0.9.28+cargo-0.75.0
+
+rustup --version
+cargo --version
+rustc --version
